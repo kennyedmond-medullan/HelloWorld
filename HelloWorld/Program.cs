8534d7a3630baa84a50cea7e830cca314ca6b76c
Kenny Edmond <kedmond@medullan.com>
2014-07-21 21:15:52 -0500

    added records

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
