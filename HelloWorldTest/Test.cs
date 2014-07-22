a64655f6281214e792eaa7af61ccbbc7eff75c79
Kenny Edmond <kedmond@medullan.com>
2014-07-22 11:59:45 -0500

    add test

---
+++ HelloWorldTest/Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using HelloWorld;
+
+namespace HelloWorldTest
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			Assert.AreEqual ("Hello World!", HelloWorld.Program.CreateMessage ());
+		}
+	}
+}
+
